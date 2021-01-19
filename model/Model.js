0724c737c0438e12cc27879cc7ada6e0b6b139ac
jasonspace1220 <59045234+jasonspace1220@users.noreply.github.com>
2021-01-19 09:27:10 +0800

    8

--- model/Model.js
+++ model/Model.js
@@ -7,7 +7,8 @@ class Model {
             host: process.env.DB_HOST,
             user: process.env.DB_USER,
             password: process.env.DB_PASSWORD,
-            database: process.env.DB_DATABASE
+            database: process.env.DB_DATABASE,
+            connectionLimit : 2
         });
     }
 
